ee2c372247d544de082e568061a1463ba3271fae
HMT@WDQ <(null)>
2014-10-28 22:08:32 +0800

    Initial Commit

---
+++ MyWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWidget
+//
+//  Created by HMT on 14/10/28.
+//  Copyright (c) 2014年 MTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWidget
+//
+//  Created by HMT on 14/10/28.
+//  Copyright (c) 2014年 MTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
